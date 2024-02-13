bec5cff66f7f252770c5bc666ea86b2e5addd06f
Pudintea <najzmitea@gmail.com>
2024-02-13 15:45:47 +0700

    Update input_form_dropdown.php

--- input_form_dropdown.php
+++ input_form_dropdown.php
@@ -10,8 +10,15 @@ echo echo form_dropdown('datasaya', $data, 'nama');
 
 echo echo form_dropdown('ukuran_baju', array ('S'=>'S', 'M' => 'M', 'L' => 'L', 'XL' => 'XL'), 'XL');
 
+//==================================================================================
+// mengambil data dari database dengan result()
+    $stts_menikah = $this->M_Najzmi->get_status_menikah();
+		$e = []; // Inisialisasi array di luar loop
 
-
+		foreach($stts_menikah as $d) {
+			$e[$d->id_status_nikah] = $d->status_nikah;
+		}
+		echo form_dropdown('statu_menikah', $e, 'lajang');
 
 
 // ====================== PUDIN.MY.ID ===============================
