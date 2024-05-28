3a5e473e11fe56b5894efad71c44ecd8b4b0686b
Pudintea <najzmitea@gmail.com>
2024-05-28 12:52:05 +0700

    Create sqlite-koneksi.php

---
+++ sqlite-koneksi.php
@@ -0,0 +1,23 @@
+<? 
+
+$db['default'] = array(
+    'dsn'	=> '',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => './application/databases/db.sqlite',
+    'dbdriver' => 'sqlite3',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
