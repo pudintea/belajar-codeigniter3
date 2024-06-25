4860b7062cfd80898dfdb742d598db82dccdb0e5
Pudintea <najzmitea@gmail.com>
2024-06-25 11:22:00 +0700

    Create WARNA_array.php

---
+++ WARNA_array.php
@@ -0,0 +1,60 @@
+<?php
+
+array('#4e73df','#6610f2','#6f42c1','#e83e8c','#e74a3b','#fd7e14','#f6c23e','#1cc88a','#20c9a6','#36b9cc','#fff','#858796','#5a5c69','#4e73df','#858796','#1cc88a','#36b9cc','#f6c23e','#e74a3b','#f8f9fc','#5a5c69');
+array('#4e73df','#6610f2','#6f42c1','#e83e8c','#e74a3b','#fd7e14','#f6c23e','#1cc88a','#20c9a6','#36b9cc','#fff','#858796','#5a5c69');
+array('#4e73df','#858796','#1cc88a','#36b9cc','#f6c23e','#e74a3b','#f8f9fc','#5a5c69');
+
+array(
+	'blue' 		=> '#4e73df',
+	'indigo' 	=> '#6610f2',
+	'purple' 	=> '#6f42c1',
+	'pink' 		=> '#e83e8c',
+	'red' 		=> '#e74a3b',
+	'orange' 	=> '#fd7e14',
+	'yellow' 	=> '#f6c23e',
+	'green' 	=> '#1cc88a',
+	'teal' 		=> '#20c9a6',
+	'cyan' 		=> '#36b9cc',
+	'white' 	=> '#fff',
+	'gray' 		=> '#858796',
+	'gray-dark' => '#5a5c69',
+	'primary' 	=> '#4e73df',
+	'secondary' => '#858796',
+	'success' 	=> '#1cc88a',
+	'info' 		=> '#36b9cc',
+	'warning' 	=> '#f6c23e',
+	'danger' 	=> '#e74a3b',
+	'light' 	=> '#f8f9fc',
+	'dark' 		=> '#5a5c69'
+);
+
+array(
+	'primary' 	=> '#4e73df',
+	'secondary' => '#858796',
+	'success' 	=> '#1cc88a',
+	'info' 		=> '#36b9cc',
+	'warning' 	=> '#f6c23e',
+	'danger' 	=> '#e74a3b',
+	'light' 	=> '#f8f9fc',
+	'dark' 		=> '#5a5c69'
+);
+
+array(
+	'blue' 		=> '#4e73df',
+	'indigo' 	=> '#6610f2',
+	'purple' 	=> '#6f42c1',
+	'pink' 		=> '#e83e8c',
+	'red' 		=> '#e74a3b',
+	'orange' 	=> '#fd7e14',
+	'yellow' 	=> '#f6c23e',
+	'green' 	=> '#1cc88a',
+	'teal' 		=> '#20c9a6',
+	'cyan' 		=> '#36b9cc',
+	'white' 	=> '#fff',
+	'gray' 		=> '#858796',
+	'gray-dark' => '#5a5c69'
+);
+
+
+
+// =============================== PUDIN.MY.ID =======================================
